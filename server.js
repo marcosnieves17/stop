1aa9229c1cf33da85c846afb972c1aaef12fab22
Yamil Asusta <hello@yamilasusta.com>
2016-10-22 17:44:14 -0400

    Mocked game

--- server.js
+++ server.js
@@ -6,36 +6,55 @@ var app = express();
 var server = require('http').Server(app);
 io = io(server);
 
+var db = {};
+
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.get('/', function(req, res) {
-  res.render('home');
-});
-
-app.get('/playground', function(req, res) {
-  res.render('playground');
+app.get('/:view', function(req, res) {
+  res.render(req.params.view);
 });
 
 io.on('connection', function(socket) {
-  console.log(io);
   socket.on('join', function(info, ack) {
+    if (io.sockets.adapter.rooms[info.room] && io.sockets.adapter.rooms[info.room]['in-game']) {
+      return ack('IN GAME ALREADY');
+    }
+
     socket.join(info.room, function(err) {
       if (err) {
         console.log(err);
-        ack('ERROR');
-      } else {
-        ack('OK');
-        io.to(info.room).emit('new player', { name: info.name });
+        return ack('ERROR');
+      }
 
-        socket.on('start', function(game_details) {
-          io.to(info.room).emit('start game', { game: game_details });
+      ack('OK');
 
-          socket.on('end', function() {
-            io.to(info.room).emit('send data');
+      socket.to(info.room).emit('new player', { name: info.name });
+
+
+      socket.on('start', function(game_details) {
+        io.sockets.adapter.rooms[info.room]['in-game'] = true;
+        io.to(info.room).emit('start game', { game: game_details });
+
+        socket.on('end', function() {
+          io.to(info.room).emit('send data');
+
+          socket.on('submission', function(data, fn) {
+            if (!(info.room in db)) {
+              db[info.room] = [];
+            }
+
+            db[info.room].push(data);
+            if (db[info.room].length >= io.sockets.adapter.rooms[info.room]) {
+              // calculate score
+              // emit winners to all
+              console.log('finished');
+              delete db[info.room];
+              delete io.sockets.adapter.rooms[info.room]['in-game'];
+            }
           });
         });
-      }
+      });
     });
   });
 });
