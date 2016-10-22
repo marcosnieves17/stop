58ae35cf29d059292fc89c9adf8dae3a82698f83
Yamil Asusta <hello@yamilasusta.com>
2016-10-22 16:52:11 -0400

    Playground and fixes

--- server.js
+++ server.js
@@ -3,31 +3,41 @@ var exphbs  = require('express-handlebars');
 var io = require('socket.io');
 
 var app = express();
-io = io(app);
+var server = require('http').Server(app);
+io = io(server);
 
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('home');
+});
+
+app.get('/playground', function(req, res) {
+  res.render('playground');
 });
 
 io.on('connection', function(socket) {
   console.log(io);
-  socket.on('join', function(info) {
+  socket.on('join', function(info, ack) {
     socket.join(info.room, function(err) {
-      console.log(err);
-      io.to(info.room).emit('new player', { name: info.name });
+      if (err) {
+        console.log(err);
+        ack('ERROR');
+      } else {
+        ack('OK');
+        io.to(info.room).emit('new player', { name: info.name });
 
-      socket.on('start', function(game_details) {
-        io.to(info.room).emit('start game', { game: game_details });
+        socket.on('start', function(game_details) {
+          io.to(info.room).emit('start game', { game: game_details });
 
-        socket.on('end', function() {
-          io.to(info.room).emit('send data');
+          socket.on('end', function() {
+            io.to(info.room).emit('send data');
+          });
         });
-      });
+      }
     });
   });
 });
 
-app.listen(3000);
+server.listen(3000);
