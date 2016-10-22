7250246d0a0d085f007471bc1a4dbc7079150bfc
David Bartolomei <david@dbartolomei.com>
2016-10-22 19:45:29 -0400

    handling empty subs

--- test/gameScore.test.js
+++ test/gameScore.test.js
@@ -4,8 +4,8 @@ var fixtures = [
     {
         name: 'bob',
         subs: {
-            'color': 'red',
-            'sex': 'male'
+            'color': '',
+            'sex': ''
         }
         // sport: 'pinga'
     }, {
@@ -37,20 +37,17 @@ var fixtures = [
         }
     }
 ];
-var result_fixtures = [{name: 'bob', subs: {color: 'red', sex: 'male'}, score: 15},
-    {
-        name: 'yamil',
-        subs: {color: 'green', sex: 'male'},
-        score: 15
-    },
-    {name: 'bob', subs: {color: 'blue', sex: 'male'}, score: 10},
-    {name: 'john', subs: {color: 'blue', sex: 'male'}, score: 10},
-    {name: 'bob', subs: {color: 'blue', sex: 'male'}, score: 10}];
+var result_fixtures = [ { name: 'bob', subs: { color: '', sex: '' }, score: 0 },
+    { name: 'yamil', subs: { color: 'green', sex: 'male' }, score: 15 },
+    { name: 'bob', subs: { color: 'blue', sex: 'male' }, score: 10 },
+    { name: 'john', subs: { color: 'blue', sex: 'male' }, score: 10 },
+    { name: 'bob', subs: { color: 'blue', sex: 'male' }, score: 10 } ];
 
 
 describe("# Game Calculator", function () {
     it("should calculate scores", function () {
         var results = stop_score(fixtures);
+        // console.log(results);
         expect(results).to.deep.equal(result_fixtures);
     })
 });
