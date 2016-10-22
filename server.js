a00458237cf3ffa6fb3dd8275aafe5e6d3a8c774
Yamil Asusta <hello@yamilasusta.com>
2016-10-22 16:31:55 -0400

    Initial socket engine

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var io = require('socket.io');
 
 var app = express();
+io = io(app);
 
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-    res.render('home');
+  res.render('home');
+});
+
+io.on('connection', function(socket) {
+  console.log(io);
+  socket.on('join', function(info) {
+    socket.join(info.room, function(err) {
+      console.log(err);
+      io.to(info.room).emit('new player', { name: info.name });
+
+      socket.on('start', function(game_details) {
+        io.to(info.room).emit('start game', { game: game_details });
+
+        socket.on('end', function() {
+          io.to(info.room).emit('send data');
+        });
+      });
+    });
+  });
 });
 
 app.listen(3000);
